1697fe61d92be6a5a04dd104867c1dfca80a892c
oyurekten <ozgury@ebi.ac.uk>
2025-09-18 18:50:55 +0000

    refactor: refactor to use some utility methods for each profile

--- gnps2mhd/config.py
+++ gnps2mhd/config.py
@@ -11,13 +11,6 @@ class Gnps2MhdConfiguration(BaseSettings):
     target_mhd_model_ms_profile_uri: str = MHD_MODEL_V0_1_MS_PROFILE_NAME
     target_mhd_model_legacy_profile_uri: str = MHD_MODEL_V0_1_LEGACY_PROFILE_NAME
 
-    public_ftp_base_url: str = (
-        "ftp://ftp.ebi.ac.uk/pub/databases/metabolights/studies/public"
-    )
-    public_http_base_url: str = (
-        "http://ftp.ebi.ac.uk/pub/databases/metabolights/studies/public"
-    )
-    study_http_base_url: str = "https://www.ebi.ac.uk/metabolights"
     default_dataset_licence_url: str = (
         "https://creativecommons.org/publicdomain/zero/1.0"
     )
--- gnps2mhd/v0_1/legacy/builder.py
+++ gnps2mhd/v0_1/legacy/builder.py
@@ -1,11 +1,8 @@
 import datetime
-import json
 import logging
 from pathlib import Path
 from urllib.parse import quote
 
-import httpx
-import xmltodict
 from mhd_model.model.v0_1.dataset.profiles.base import graph_nodes as mhd_domain
 from mhd_model.model.v0_1.dataset.profiles.base.dataset_builder import MhDatasetBuilder
 from mhd_model.model.v0_1.dataset.profiles.base.profile import MhDatasetBaseProfile
@@ -19,10 +16,15 @@ from mhd_model.model.v0_1.rules.managed_cv_terms import (
     COMMON_PROTOCOLS,
     COMMON_STUDY_FACTOR_DEFINITIONS,
 )
-from mhd_model.shared.model import CvTerm, Revision, UnitCvTerm
+from mhd_model.shared.model import CvTerm, Revision
 from pydantic import AnyUrl, HttpUrl
 
 from gnps2mhd.config import Gnps2MhdConfiguration
+from gnps2mhd.v0_1.utils import (
+    create_cv_term_object,
+    create_cv_term_value_object,
+    fetch_massive_metadata_file,
+)
 
 logger = logging.getLogger(__name__)
 
@@ -80,94 +82,6 @@ FILE_EXTENSIONS: dict[tuple[str, bool], CvTerm] = {
     (".mzxml", False): CvTerm(source="EDAM", accession="EDAM:3654", name="mzXML"),
     (".ibd", False): CvTerm(source="EDAM", accession="EDAM:3839", name="ibd"),
 }
-
-
-def create_cv_term_object(
-    type_: str, accession: str, source: str, name: str
-) -> mhd_domain.CvTermObject:
-    if not source or not accession:
-        return mhd_domain.CvTermObject(type_=type_, name=name)
-
-    return mhd_domain.CvTermObject(
-        type_=type_, accession=accession, source=source, name=name
-    )
-
-
-def create_cv_term_value_object(
-    type_: str,
-    accession: str = "",
-    source: str = "",
-    name: str = "",
-    value: None | str = None,
-    unit: None | UnitCvTerm = None,
-) -> mhd_domain.CvTermValueObject:
-    unit_cv = None
-    if unit:
-        if not source or not accession:
-            unit_cv = UnitCvTerm(name=unit.name) if unit else None
-
-    if not source or not accession:
-        return mhd_domain.CvTermValueObject(
-            type_=type_, name=name, value=value, unit=unit_cv
-        )
-
-    return mhd_domain.CvTermValueObject(
-        type_=type_,
-        accession=accession,
-        source=source,
-        name=name,
-        value=value,
-        unit=unit_cv,
-    )
-
-
-def fetch_massive_metadata_file(
-    massive_study_id: str, cache_root_path: None | str = None
-):
-    cache_path = None
-    if cache_root_path:
-        cache_path = Path(f"{cache_root_path}/{massive_study_id}.params.xml.json")
-        try:
-            if cache_path.exists():
-                with cache_path.open() as f:
-                    json_data = json.load(f)
-                return json_data
-        except Exception as ex:
-            logger.error("Error loading from cache. %s", ex)
-            pass
-
-    metadata_file_name = "ccms_parameters/params.xml"
-    metadata_http_file_url = (
-        "https://massive.ucsd.edu/ProteoSAFe/DownloadResultFile"
-        f"?file=f.{massive_study_id}%2F{quote(metadata_file_name, safe='')}"
-    )
-    try:
-        response = httpx.get(metadata_http_file_url, timeout=5)
-        response.raise_for_status()
-
-        # Parse XML to OrderedDict
-        json_data = xmltodict.parse(response.text)
-        params = json_data.get("parameters", {}).get("parameter", [])
-        params_dict = {}
-        for x in params:
-            name = x.get("@name")
-            val = x.get("#text")
-            if name not in params_dict:
-                params_dict[name] = val
-            else:
-                if isinstance(params_dict[name], list):
-                    params_dict[name].append(val)
-                else:
-                    params_dict[name] = [val]
-        if cache_path:
-            with cache_path.open("w") as f:
-                json_data = json.dump(params_dict, f, indent=4)
-        return params_dict
-
-    except Exception as e:
-        logger.error("Error fetching massive metadata file %s: %s", massive_study_id, e)
-
-    return None
 
 
 class MhdLegacyDatasetBuilder:
